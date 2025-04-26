96919634efd6947590cd599afa219bae75b3cb10
yuanxiesa <40753490+yuanxiesa@users.noreply.github.com>
2025-04-26 10:38:59 -0500

    update data structure

--- data_collection/citing_paper_2nd_collection.py
+++ data_collection/citing_paper_2nd_collection.py
@@ -4,16 +4,18 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Filter and save specific columns from a CSV file.")
     parser.add_argument("input_file", help="Path to the input CSV file")
+    parser.add_argument("oc_file", help="Path to the shortened oc CSV file")
+    parser.add_argument("output_dir", help="Path to the output directory")
     args = parser.parse_args()
 
-    oc = pd.read_csv('short_2007_open_citations_curated.csv')
+    oc = pd.read_csv(args.oc_file)
     doi_df = pd.read_csv(args.input_file)
     for row in doi_df.itertuples(index=True, name="Row"):
         prefix = row[1]
         doi = row[2]
         selection_df = oc.loc[oc['cited'] == doi,]
-        outfile = f'lp_2nd_dir_0401/{prefix}.csv'
+        outfile = f'{args.output_dir}/{prefix}.csv'
         selection_df.to_csv(outfile, index=False)
-
+        
 if __name__ == '__main__':
     main()
